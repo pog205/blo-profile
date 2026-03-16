d12beed0f49235d34b9d0594f156e48dc2ec816e
pog205 <phongdtph48650@gmail.com>
2026-03-16 21:24:08 +0700

    feat(migrations): add initial Postgres migration with background effects and bio profiles

---
+++ src/backend/BioProfile.Infrastructure/Data/Migrations/Postgres/20260316063826_InitialPostgres_Final1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BioProfile.Infrastructure.Data.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class InitialPostgres_Final1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
